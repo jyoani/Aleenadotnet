cbf938b6c1e54e8029b9a863296e88793e9eb319
Jyothi Krishna <anishjyothikrishna@gmail.com>
2024-06-25 11:08:23 +0400

    error

--- BulkyProject/Program.cs
+++ BulkyProject/Program.cs
@@ -12,8 +12,6 @@ using Azure.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
-var keyVaultEndpoint = new Uri(Environment.GetEnvironmentVariable("VaultUri"));
-builder.Configuration.AddAzureKeyVault(keyVaultEndpoint, new DefaultAzureCredential());
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
